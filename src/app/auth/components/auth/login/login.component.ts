d0463d768360bdcce6b7a86b510bb0b556eabed2
akshatt2305 <namdevakshat2004@gmail.com>
2025-02-15 15:53:25 +0530

    validators

--- src/app/auth/components/auth/login/login.component.ts
+++ src/app/auth/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required]],
+    });
+  }
 
 }
