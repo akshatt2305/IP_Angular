e5719029f782cf1a0b23358fb48afa2aa36e28fc
akshatt2305 <namdevakshat2004@gmail.com>
2025-02-15 16:23:48 +0530

    login_component

--- src/app/auth/components/auth/login/login.component.ts
+++ src/app/auth/components/auth/login/login.component.ts
@@ -12,8 +12,8 @@ export class LoginComponent {
   loginForm: FormGroup;
   constructor(private fb: FormBuilder){
     this.loginForm = this.fb.group({
-      email:['',[Validators.required, Validators.email]],
-      password:['',[Validators.required]],
+      email:['',[ Validators.required,Validators.email]],
+      password:['',[Validators.required , Validators.minLength(5)]],
     });
   }
 
